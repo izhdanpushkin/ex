db465e75ff82f460dcc6c6745a25bebad61fbda6
Azuny <crippyua@gmail.com>
2016-11-27 19:37:31 +0200

    Add overlay to all images

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 overlay = $("#overlay");
 
-$('.test').mouseenter(function(){
+$('.screened').mouseenter(function(){
+    overlay.hide(); // Removes some weird behavior
     overlay.css({"width":"", "height":"", "top":"", "left":""});
     img = $(this);
-    width = parseInt(img.css("width")) + 1 + 'px';
-    height = parseInt(img.css("height")) + 1 + 'px';
-    off_top = img.offset().top - 1 + "px";
-    off_left = img.offset().left - 1 + "px";
+    width = parseInt(img.css("width")) + 'px';
+    height = parseInt(img.css("height")) + 'px';
+    // Rounding down so no overflow occurs
+    off_top = Math.floor(img.offset().top) + "px";
+    off_left = Math.floor(img.offset().left) + "px";
     overlay.css({"width":width, "height":height, "top":off_top, "left":off_left});
+    overlay.show();
 });
 
 $(document).ready(function() {
