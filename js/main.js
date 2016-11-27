4b9f39cf5520167b907e69225591feb228753728
Azuny <crippyua@gmail.com>
2016-11-27 09:00:11 +0200

    Change test overlay transition to css

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 overlay = $("#overlay");
 
 $('.test').mouseenter(function(){
+    overlay.css({"width":"", "height":"", "top":"", "left":""});
     img = $(this);
-    overlay.hide(); // Allows to fade in slowly
     width = parseInt(img.css("width")) + 1 + 'px';
     height = parseInt(img.css("height")) + 1 + 'px';
     off_top = img.offset().top + "px";
     off_left = img.offset().left + "px";
-    overlay.css({"width":width, "height":height, "top":off_top, "left":off_left}).fadeIn(250);
+    overlay.css({"width":width, "height":height, "top":off_top, "left":off_left});
 });
-// Find better way to cancel overlay, since quick doesn't trigger entering overlay, thus resulting in no mouseleave event.
-$('.overlay').mouseleave(function(){
-    overlay.css({"width":"", "height":"", "top":"", "left":""});
-    console.log('left overlay');
-});
-// Using css :hover seems like a nice idea, come back to that later
